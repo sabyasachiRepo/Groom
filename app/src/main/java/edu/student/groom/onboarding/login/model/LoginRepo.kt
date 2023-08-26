4ddd6ff833e749175b6345d904561936d9334a08
sabya <sabyasachi.biswal1@gmail.com>
2023-08-26 23:32:49 +0800

    used flow

--- app/src/main/java/edu/student/groom/onboarding/login/model/LoginRepo.kt
+++ app/src/main/java/edu/student/groom/onboarding/login/model/LoginRepo.kt
@@ -1,10 +1,16 @@
 package edu.student.groom.onboarding.login.model
 
 import edu.student.groom.onboarding.login.model.service.LoginService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class LoginRepo(private val loginService: LoginService = LoginService()) {
 
-    suspend fun logIn(loginRequest: LoginRequest):LoginResponse{
+   /* suspend fun logIn(loginRequest: LoginRequest):LoginResponse{
         return loginService.loginApi.login(loginRequest)
+    }*/
+
+    fun logIn(loginRequest: LoginRequest): Flow<LoginResponse> = flow {
+        emit(loginService.loginApi.login(loginRequest))
     }
 }
