ac68f3e1e94e3d1f7c90ac37ccce65ac79af51f5
sabya <sabyasachi.biswal1@gmail.com>
2023-09-28 21:35:03 +0800

    dagger dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ plugins {
     id("com.android.library") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+
 }
 
 tasks.register<Delete>("clean").configure {
