72fb9da31e76129f509723cef3a6c95e1cd423f1
sabya <sabyasachi.biswal1@gmail.com>
2023-10-10 21:57:00 +0800

    Network module for DI added

--- app/src/main/java/edu/student/groom/di/NetworkModule.kt
+++ app/src/main/java/edu/student/groom/di/NetworkModule.kt
@@ -2,7 +2,6 @@ package edu.student.groom.di
 
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.components.SingletonComponent
 
 @Module
